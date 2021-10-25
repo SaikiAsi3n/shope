3e04f5ee338bd2f3ac40abc06fb3cc3830a8de80
Nam Phùng <91862644+SaikiAsi3n@users.noreply.github.com>
2021-10-26 00:39:59 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -16,3 +16,23 @@ function backslide() {
     var img=["./img/qc3.jpg" , "./img/qc4.jpg" , "./img/qc5.jpg"];
     document.getElementById('img__to').src = img[index];
 }
+
+var sl=0;
+function giohang(){
+    sl++;
+    document.getElementById("giohang__number").innerHTML = sl;
+}
+
+var img = document.getElementById('myImg');
+        var modal = document.getElementById('myModal');                   
+        var modalImg    = document.getElementById("img01");
+        var captionText = document.getElementById("caption");
+        img.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        }         
+        var span = document.getElementsByClassName("close")[0];
+          span.onclick = function() { 
+            modal.style.display = "none";
+          }
